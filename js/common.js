7b785f9123d83e6c1d3e9fe253719220fae04e35
Georgiy <goshazvir@gmail.com>
2013-04-05 13:57:21 +0300

    :(

---
+++ js/common.js
@@ -0,0 +1,50 @@
+﻿// for circular scrollable
+
+$.fn.scrollableAddClones = function(addItems) {
+    // grab scrollable plugin
+    var scrollable;
+    if (!(scrollable = $(this).data('scrollable')) || !scrollable.getConf().circular)
+    return;
+    // grab scrollable elements and remember it's count
+    var nodes = scrollable.getItems();
+    var length = nodes.length;
+
+    // grab class for the nodes
+    var clonedClass = scrollable.getConf().clonedClass;
+    // get wrap object to append the clones to
+    var wrap = scrollable.getItemWrap();
+    // fill as much nodes as needed for 500 pixels
+    if (!addItems) addItems = Math.ceil(1000 / nodes.eq(1).width());
+    // create fake container to add the clones to (max 15 clones)
+    var newNodesAppend = $('<span />');
+    for (var i = 1; i <= (addItems < 15 ? addItems : 15); i++)
+    nodes.eq(i % length).clone().addClass(clonedClass).appendTo(newNodesAppend);
+    // insert HTML
+    newNodesAppend.children().appendTo(wrap);
+}
+
+$(document).ready(function() {
+
+	// scrollable
+	$('.picture-day').scrollable({
+		next:'.picture-day__next',
+		prev:'.picture-day__prew',
+		circular:true
+	});
+
+    //
+	$(".picture-day").scrollableAddClones();
+
+    $('.list-news li').click(function(){
+        $('.super-news').html($(this).html());
+        return false;
+    })
+    //
+    jQuery(".top1").hover(
+       function() {
+          jQuery(".test1").css("display","block");
+       },
+       function() {
+          jQuery(".test1").css("display","none");
+       });
+})
